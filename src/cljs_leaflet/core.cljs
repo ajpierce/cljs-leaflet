104a9341a8d2fd1517d63b0bbad217ad4cb1c739
Andrew J. Pierce <andrew.j.pierce@gmail.com>
2016-04-10 19:06:19 -0400

    Add reactive rendering with L.geoJson and point management in the atom
    Additionally, replace markers with circleMarkers so we don't need to use
    image assets from a CDN.

--- src/cljs_leaflet/core.cljs
+++ src/cljs_leaflet/core.cljs
@@ -5,6 +5,7 @@
 
 (defonce map-info {
    :center [51.505 -0.09]
+   :zoom 13
    :tiles {
            :url "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw",
            :options {
@@ -22,31 +23,78 @@
     :iconSize [25 41]
     :iconAnchor [12 41] }))})
 
+;; cljsjs doesn't come with image assets, so substitute with circlemarker settings
+(defonce gj-marker-settings
+  {:radius 8
+   :fillColor "#ff7800"
+   :color "#000"
+   :weight 1
+   :opacity 1
+   :fillOpacity 0.8} )
+
+(defn point-to-layer [feature, latlng]  ;; http://leafletjs.com/examples/geojson.html
+  (.circleMarker js/L latlng (clj->js gj-marker-settings)))
+
 ;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {}))
+(defonce app-state
+  (atom {:latest-id 0
+         :map-layers {} }))
 
 (defonce main-map
-  (.setView (.map js/L "map") (clj->js (:center map-info)) 13))
+  (.setView (.map js/L "map") (clj->js (:center map-info)) (:zoom map-info)))
 
 (defonce tile-layer
   (.tileLayer js/L
               (:url (:tiles map-info))
               (clj->js (:options (:tiles map-info))) ))
 
-(defn setup-map! []
-  (println "Adding tiles to the map")
-  (.addTo tile-layer main-map))
+(defonce vector-layer (.layerGroup js/L))
 
-(setup-map!)
+(defn setup-map! [lmap]
+  (.addTo tile-layer lmap)
+  (.addTo vector-layer lmap))
 
-(defn add-point! [lmap coords]
+(setup-map! main-map)
+
+;; -- Function to add a point directly to the map -- ;;
+(defn add-point-directly! [layer coords]
   (println (str "Adding point to map at coords" coords))
   (.addTo (.marker js/L
                    (clj->js coords)
                    (clj->js map-icon))
-          lmap))
+          layer))
+;;(add-point-directly! vector-layer [51.5, -0.09])
+;; -------------------------------------------------- ;;
 
-(add-point! main-map [51.5, -0.09])
+;; Gets a unique identifier for layers by incrementing an int in the app state
+(defn get-layer-id! []
+  (:latest-id (swap! app-state update-in [:latest-id] inc)))
+
+;; Functions to swap data into (or out of) our atom
+(defn add-point! [point-data]
+  (swap! app-state assoc-in [:map-layers (get-layer-id!)] point-data))
+
+(defn remove-point! [id]
+  (println (str "Removing point " id))
+  (swap! app-state update-in [:map-layers] dissoc id))
+
+(defn create-geojson [features]
+  (let [fc {:type "FeatureCollection" :features features}
+        fcjs (clj->js fc)]
+    (.geoJson js/L fcjs
+                   (clj->js {:pointToLayer point-to-layer}) )))
+
+ ;; Listens for state changes in the atom and updates the map accordingly
+(add-watch app-state :map-layers-watcher
+  (fn [r k old-state new-state]
+    (when (not= (:map-layers old-state) (:map-layers new-state))
+      (let [features (vals (:map-layers new-state))
+            gj (create-geojson features)]
+        ;; Clear map layers and re-add the new layer
+        (.clearLayers vector-layer)
+        (.addLayer vector-layer gj) ))))
+
+(add-point! {:coordinates [-0.09, 51.5] :type "Point"})
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
